4c6b3c790987b77cd43367808b452b56e7ff95ed
Boli27 <judiemu27@gmail.com>
2025-01-16 11:42:52 -0500

    parte 2

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -4,7 +4,7 @@ let books = require("./booksdb.js");
 const regd_users = express.Router();
 
 let users = [];
-
+const SECRET_KEY = "mi_clave_secreta";
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
 }
@@ -14,9 +14,28 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Validar que los campos estén completos
+  if (!username || !password) {
+      return res.status(400).json({ message: "Por favor, ingrese todos los datos" });
+  }
+
+  // Verificar si el usuario existe y las credenciales son correctas
+  const user = users.find(user => user.username === username && user.password === password);
+  if (!user) {
+      return res.status(401).json({ message: "Nombre de usuario o contraseña incorrectos" });
+  }
+
+  // Crear un JWT
+  const token = jwt.sign({ username: user.username }, SECRET_KEY, { expiresIn: '1h' });
+
+  // Enviar el token como respuesta
+  return res.status(200).json({
+      message: "Inicio de sesión exitoso",
+      token
+  });
 });
 
 // Add a book review
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,11 +4,21 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+public_users.post("/register", (req, res) => {
+    const { username, password } = req.body; 
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    if (!username || !password) {
+        return res.status(400).json({ message: "Por favor, ingrese todos los datos" });
+    }
+
+    const userExists = users.find(user => user.username === username);
+    if (userExists) {
+        return res.status(400).json({ message: "Este usuario ya existe" });
+    }
+    users.push({ username, password });
+    return res.status(201).json({ message: "Usuario registrado correctamente" });
 });
+
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
@@ -19,25 +29,41 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const book = books[isbn] || null;
+
+
+  return book ? res.send(book): res.send("Libro no encontrado");
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const book = JSON.stringify(
+    Object.values(books).filter(book => book.author === author),
+    null,
+    2
+);
+  return book ? res.send(book): res.send("Libro no encontrado");
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  const book = JSON.stringify(
+    Object.values(books).filter(book => book.title === title),
+    null,
+    2
+);
+  return book ? res.send(book): res.send("Libro no encontrado");
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const book = books[isbn] || null;
+
+  return book ? res.json({reviews: book.reviews}): res.send("Libro no encontrado");
 });
 
 module.exports.general = public_users;
